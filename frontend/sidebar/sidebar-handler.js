daa126e71bda6b1dbafeb94933e7258aab715e28
GustavoNogai <gustavo.nogaisaito@usp.br>
2025-01-28 13:07:35 -0300

    Reorganize public files and adjust paths

--- frontend/sidebar/sidebar-handler.js
+++ frontend/sidebar/sidebar-handler.js
@@ -1,7 +1,7 @@
 const sidebar = document.getElementById("sidebar");
 const columnRight = document.getElementsByClassName("column right")[0];
 
-
+// Aumenta a barra lateral e diminui a tela principal ao passar o mouse para dentro
 sidebar.addEventListener("mouseenter", () => {
     //console.log("Object: opening sidebar");
     sidebar.style.width = "250px";
@@ -9,6 +9,7 @@ sidebar.addEventListener("mouseenter", () => {
     columnRight.style.width = "calc(100% - 250px)";
 });
 
+// Diminui a barra lateral e aumenta a tela principal ao passar o mouse para fora
 sidebar.addEventListener("mouseleave", () => {
     //console.log("Object: closing sidebar");
     sidebar.style.width = "85px";
--- frontend/sidebar/sidebar.js
+++ frontend/sidebar/sidebar.js
@@ -1,10 +1,12 @@
 const sidebar = document.getElementById("mySidebar");
 
+// Aumenta a barra lateral ao passar o mouse para dentro
 sidebar.addEventListener("mouseenter", () => {
     //console.log("Opening sidebar");
     sidebar.style.width = "250px";
 });
 
+// Diminui a barra lateral ao passar o mouse para fora
 sidebar.addEventListener("mouseleave", () => {
     //console.log("Closing sidebar");
     sidebar.style.width = "85px";
